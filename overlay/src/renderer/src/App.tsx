f4d38b5ec3ac5c0efc2b4d26637e327e0428ee26
misapavek <misapavek@seznam.cz>
2026-02-04 23:09:48 +0100

    hop

--- overlay/src/renderer/src/App.tsx
+++ overlay/src/renderer/src/App.tsx
@@ -1,9 +1,11 @@
+
 import { Overlay } from "./components/Overlay"
 import { useLeagueGame } from "./hooks/useLeagueGame"
 
 function App() {
 
   const {gameData, isInGame} = useLeagueGame()
+  
   return (
     <div className="App">
       <Overlay isInGame={isInGame} gameData={gameData} />
